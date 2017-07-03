8a66c9cc3705b529c4281471d64d49b7b27857ed
calvin echols <calvin.echols@gmail.com>
2017-07-02 23:00:38 -0400

    getting to a good place!

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let isProd = () => {
+    let env = process.env.NODE_ENV;
+    if(env) {
+        return env.toUpper() === "PROD";
+    } 
+    return false;
+};
+
+let htmlMinifyOptions = isProd() ? {
+    //TODO: Add minify options here: https://github.com/kangax/html-minifier#options-quick-reference
+    collapseWhitespace: true
+} : false;
+
+let htmlWebpackPlugin = new HtmlWebpackPlugin({
+    title: `${process.env.APPNAME}`,
+    template: "./templates/index.ejs",
+    minify: htmlMinifyOptions,
+    hash: true
+});
+
 var config = {
-    context: path.resolve(__dirname, "app"),
+    context: path.resolve(__dirname, "src"),
     entry: {
-        index: "./app.js"
-    }
+        index: "./index.js"
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "./js/[name].bundle.js",
+        publicPath: "/"
+    },
+    plugins: [
+        htmlWebpackPlugin
+    ]
 }
 
-module exports = config;
+module.exports = config;
