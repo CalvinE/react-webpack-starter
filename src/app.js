55ca07e03f38b99d1283b57c7c72020ab5c65eea
calvin echols <calvin.echols@gmail.com>
2017-08-05 14:01:29 -0400

    got sass bundleing working!

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+import './sass/index.scss';
+
+// ReactDom.render(
+//     <h1>Howdy!</h1>,
+//     document.getElementById('root')
+// );
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,58 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 let isProd = () => {
     let env = process.env.NODE_ENV;
     if(env) {
-        return env.toUpper() === "PROD";
+        return env.toUpperCase() === "PROD";
     } 
     return false;
 };
 
 let htmlMinifyOptions = isProd() ? {
-    //TODO: Add minify options here: https://github.com/kangax/html-minifier#options-quick-reference
+    // Add minify options here: https://github.com/kangax/html-minifier#options-quick-reference
     collapseWhitespace: true
 } : false;
 
 let htmlWebpackPlugin = new HtmlWebpackPlugin({
+    // Add configuration options here: https://github.com/jantimon/html-webpack-plugin#configuration
     title: `${process.env.APPNAME}`,
     template: "./templates/index.ejs",
     minify: htmlMinifyOptions,
     hash: true
 });
 
+// let scssExtractTextPlugin = ;
+
 var config = {
     context: path.resolve(__dirname, "src"),
     entry: {
-        index: "./index.js"
+        app: "./app.js"
     },
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "./js/[name].bundle.js",
         publicPath: "/"
     },
+    module: {
+        rules: [
+            { // SASS loader
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
     plugins: [
-        htmlWebpackPlugin
+        htmlWebpackPlugin,
+        new ExtractTextPlugin({
+            filename: 'css/[name].bundle.css',
+            disable: !isProd(),
+            allChunks: true
+        })
     ]
 }
 
