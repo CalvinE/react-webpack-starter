b86ea0fc86302dc50124442f59b7e9d2537c7a0c
calvin echols <calvin.echols@gmail.com>
2017-08-19 23:35:42 -0400

    got some eslint rules in place.

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,4 @@
+/* global document */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -6,11 +7,11 @@ import defaultImage from './images/default.png';
 import './sass/index.scss';
 
 ReactDOM.render(
-    (
-        <div>
-            <h1>Howdy!</h1>
-            <img src={defaultImage} />
-        </div>
-    ),
-    document.getElementById('root')
+	(
+		<div>
+			<h1>Howdy!</h1>
+			<img alt="This is a default image." src={defaultImage} />
+		</div>
+	),
+	document.getElementById('root'),
 );
--- webpack.config.js
+++ webpack.config.js
@@ -4,152 +4,147 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 
-let isProd = () => {
-    let env = process.env.NODE_ENV;
-    if(env) {
-        return env.toUpperCase() === "PROD";
-    } 
-    return false;
+const isProd = () => {
+	const env = process.env.NODE_ENV;
+	if (env) {
+		return env.toUpperCase() === 'PROD';
+	}
+	return false;
 };
 
-let shouldGenerateSourceMaps = () => {
-    return true; // Why not always generate them?
-    // if(process.env.OVERRIDE_SOURCE_MAP === "true"){
-    //     return true;
-    // } else if (!isProd() === true) {
-    //     return true;
-    // } else {
-    //     return false;
-    // }
-};
+const shouldGenerateSourceMaps = () =>
+	true // Why not always generate them?
+	// if(process.env.OVERRIDE_SOURCE_MAP === "true"){
+	//     return true;
+	// } else if (!isProd() === true) {
+	//     return true;
+	// } else {
+	//     return false;
+	// }
+;
 
-console.log(`isProd = ${isProd()}`);
-console.log(`shouldGenerateSourceMaps = ${shouldGenerateSourceMaps()}`);
-
-let htmlMinifyOptions = isProd() ? {
-    // Add minify options here: https://github.com/kangax/html-minifier#options-quick-reference
-    collapseWhitespace: true
+const htmlMinifyOptions = isProd() ? {
+	// Add minify options here: https://github.com/kangax/html-minifier#options-quick-reference
+	collapseWhitespace: true,
 } : false;
 
-let htmlWebpackPlugin = new HtmlWebpackPlugin({
-    // Add configuration options here: https://github.com/jantimon/html-webpack-plugin#configuration
-    title: `${process.env.APPNAME}`,
-    template: "./templates/index.ejs",
-    minify: htmlMinifyOptions,
-    hash: true
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+	// Add configuration options here: https://github.com/jantimon/html-webpack-plugin#configuration
+	title: `${process.env.APPNAME}`,
+	template: './templates/index.ejs',
+	minify: htmlMinifyOptions,
+	hash: true,
 });
 
-let extractSass = new ExtractTextPlugin({
-    filename: "css/[name].[contenthash].css",
-    disable: !isProd(),
-    allChunks: true
+const extractSass = new ExtractTextPlugin({
+	filename: 'css/[name].[contenthash].css',
+	disable: !isProd(),
+	allChunks: true,
 });
 
 // let scssExtractTextPlugin = ;
 
-var config = {
-    devtool: isProd() ? 'source-map' : 'inline-source-map',
-    context: path.resolve(__dirname, "src"),
-    entry: {
-        app: "./app.jsx"
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "js/[name].bundle.js",
-        publicPath: "/"
-    },
-    module: {
-        rules: [
-            { // SASS loader
-                test: /\.scss$/,
-                use: extractSass.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        { 
-                            loader: 'css-loader', 
-                            options: {
-                                sourceMap: shouldGenerateSourceMaps()
-                            }
-                        },
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                sourceMap: shouldGenerateSourceMaps(),
-                                plugins: () => {
-                                    return [
-                                        autoprefixer({
-                                            // Options go here...
-                                        })
-                                    ];
-                                }
-                            }
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: shouldGenerateSourceMaps(),
-                                outputStyle: 'compressed'
-                            }
-                        }
-                    ]
-                })
-            },
-            {
-                test: /\.tsx$/,
-                use: [
+const config = {
+	devtool: isProd() ? 'source-map' : 'inline-source-map',
+	context: path.resolve(__dirname, 'src'),
+	entry: {
+		app: './app.jsx',
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'js/[name].bundle.js',
+		publicPath: '/',
+	},
+	module: {
+		rules: [
+			{ // SASS loader
+				test: /\.scss$/,
+				use: extractSass.extract({
+					fallback: 'style-loader',
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								sourceMap: shouldGenerateSourceMaps(),
+							},
+						},
+						{
+							loader: 'postcss-loader',
+							options: {
+								sourceMap: shouldGenerateSourceMaps(),
+								plugins: () => [
+									autoprefixer({
+										// Options go here...
+									}),
+								],
+							},
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: shouldGenerateSourceMaps(),
+								outputStyle: 'compressed',
+							},
+						},
+					],
+				}),
+			},
+			{
+				test: /\.tsx$/,
+				use: [
 
-                ]
-            },
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['es2015', 'react']
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'images/'
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'fonts/'
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        htmlWebpackPlugin,
-        extractSass,
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true
-        })//,
-        // new webpack.LoaderOptionsPlugin({
-        //     options: {
-        //         postcss: [
-        //             autoprefixer()
-        //         ]
-        //     }
-        // })
-    ]
-}
+				],
+			},
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['es2015', 'react'],
+						},
+					},
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							outputPath: 'images/',
+						},
+					},
+				],
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							outputPath: 'fonts/',
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		htmlWebpackPlugin,
+		extractSass,
+		new webpack.optimize.UglifyJsPlugin({
+			sourceMap: true,
+		}), // ,
+		// new webpack.LoaderOptionsPlugin({
+		//     options: {
+		//         postcss: [
+		//             autoprefixer()
+		//         ]
+		//     }
+		// })
+	],
+};
 
 module.exports = config;
