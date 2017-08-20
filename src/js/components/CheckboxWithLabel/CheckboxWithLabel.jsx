4956c0a4c357574f1f077731050905c6dae2b5cb
calvin echols <calvin.echols@gmail.com>
2017-08-20 00:31:39 -0400

    Added testing.

---
+++ src/js/components/CheckboxWithLabel/CheckboxWithLabel.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class CheckboxWithLabel extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { isChecked: false };
+
+		// bind manually because React class components don't auto-bind
+		// http://facebook.github.io/react/blog/2015/01/27/react-v0.13.0-beta-1.html#autobinding
+		this.onChange = this.onChange.bind(this);
+	}
+
+	onChange() {
+		this.setState({ isChecked: !this.state.isChecked });
+	}
+
+	render() {
+		return (
+			<label>
+				<input
+					type="checkbox"
+					checked={this.state.isChecked}
+					onChange={this.onChange}
+				/>
+				{this.state.isChecked ? this.props.labelOn : this.props.labelOff}
+			</label>
+		);
+	}
+}
+
+CheckboxWithLabel.propTypes = {
+	labelOn: PropTypes.string,
+	labelOff: PropTypes.string
+};
---
+++ src/js/components/CheckboxWithLabel/CheckboxWithLabel.test.jsx
@@ -0,0 +1,18 @@
+/* global describe, it */
+
+import * as Chai from 'chai';
+import React from 'react';
+import { shallow } from 'enzyme';
+import CheckboxWithLabel from './CheckboxWithLabel';
+
+describe('', () => {
+	it('Component should have on and off labels that are switched based on state.', () => {
+		const checkbox = shallow(
+			<CheckboxWithLabel labelOn="On" labelOff="Off" />
+		);
+
+		Chai.expect(checkbox.text()).to.equal('Off');
+		checkbox.find('input').simulate('change');
+		Chai.expect(checkbox.text()).to.equal('On');
+	});
+});
